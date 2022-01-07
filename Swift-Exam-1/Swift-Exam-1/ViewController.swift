96cad48f4a7a05166e6d9c479c544a8610071551
Adem <adem@adem>
2022-01-07 17:20:26 +0200

    Init

---
+++ Swift-Exam-1/Swift-Exam-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Exam-1
+//
+//  Created by Adem Tsranchaliev on 7.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
